df5a48624b6ec05cca552b85d7763f0a7a0b4e6c
Pierre-François Gimenez <pierre-francois.gimenez@centralesupelec.fr>
2022-03-18 17:15:23 +0100

    Readability

--- jh.py
+++ jh.py
@@ -1,3 +1,4 @@
+# A number indicates a confirmed depth
 uniques = [["Exosuit",6],
            ["Shadowcloak",4],
            ["Cybersuit",5],
@@ -29,12 +30,14 @@ uniques = [["Exosuit",6],
            ["Vulcan"],
            ["Wavedancer"]]
 
-additional_paths = [["URR","EUROPA"],
-                    ["RRUU","IO"]]
+# You can manually add new paths here
+additional_paths = [["URR","Europa portal"],
+                    ["RRUU","Io portal"]]
 
 codes = ["U","R","D","L"]
-default_length=4
+default_length=7
 
+# generate the prefix tree (recursive function)
 def add(dic, seq, name):
     if len(seq)==0:
         if dic.get("here")==None:
@@ -47,6 +50,7 @@ def add(dic, seq, name):
             dic[seq[0]]=nextdic
         add(nextdic, seq[1:], name)
 
+# transform the name into URDL sequence
 def name2seq(u,length):
     u=u.lower().strip().replace(" ", "")[:length]
     prev=(ord(u.lower().strip()[0])-ord('a'))//7
@@ -56,6 +60,7 @@ def name2seq(u,length):
         out.append(codes[prev])
     return out
 
+# export to graphviz format
 def export(dic):
     if dic.get("here") is None:
         print(str(id(dic))+" [shape=point, label=\"\"]")
@@ -63,21 +68,25 @@ def export(dic):
         if k=="here":
             if len(v)==1:
                 v=v[0]
-            print(str(id(dic))+" [label=\""+str(v)+"\"]")
+            if str(v).endswith("portal"):
+                print(str(id(dic))+" [shape=box, label=\""+str(v)+"\"]")
+            else:
+                print(str(id(dic))+" [label=\""+str(v)+"\"]")
         else:
             print(str(id(dic))+" -> "+str(id(v))+" [label=\""+k+"\"]")
             export(v)
 
 dic = {}
 for u in uniques:
-    name = u[0] if len(u)>1 else u[0]+" UNCONFIRMED"
+    name = u[0] if len(u)>1 else u[0]+"\nUNCONFIRMED"
     length = u[1] if len(u)>1 else default_length
     add(dic, name2seq(name,length), name)
+
 for p in additional_paths:
     add(dic,list(p[0]),p[1])
 
 print("digraph {")
 print("label=\"Jupiter Hell Purgatory Map\"")
 export(dic)
-print(str(id(dic))+" [label=\"START\"]")
+print(str(id(dic))+" [shape=pentagon label=\"START\"]")
 print("}")
